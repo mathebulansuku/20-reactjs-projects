e65a29c8e686e930e917555a0d8cadd1813c76b6
Nsuku Mathebula <mathebulansuku@gmail.com>
2024-03-13 23:00:07 +0200

    New Random Color component created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Accordion from "./components/accordion/Accordion";
+import RandomColor from "./components/random-color/RandomColor";
 
 function App() {
   return (
     <div className="App">
-      <Accordion />
+      {/* <Accordion /> */}
+      <RandomColor />
     </div>
   );
 }
