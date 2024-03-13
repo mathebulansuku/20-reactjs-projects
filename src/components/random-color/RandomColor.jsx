2fcf6ef64cc4080a359a6466f981f4c98f9f7dad
Nsuku Mathebula <mathebulansuku@gmail.com>
2024-03-13 23:25:16 +0200

    Generate a random color

--- src/components/random-color/RandomColor.jsx
+++ src/components/random-color/RandomColor.jsx
@@ -4,9 +4,18 @@ export default function RandomColor() {
   const [typeOfColor, setTypeOfColor] = useState("hex");
   const [color, setColor] = useState("#000000");
 
+  function randomColorUtility(length) {
+    return Math.floor(Math.random() * length);
+  }
+
   function handleCreateRandomHexColor() {
     const hex = [1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
     let hexColor = "#";
+
+    for (let i = 0; i < 6; i++) {
+      hexColor += hex[randomColorUtility(hex.length)];
+    }
+    setColor(hexColor);
   }
   function handleCreateRandomRgbColor() {}
 
