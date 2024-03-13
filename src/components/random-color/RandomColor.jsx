c0e486237cd447bc1180816aa2bc2434ed33424d
Nsuku Mathebula <mathebulansuku@gmail.com>
2024-03-13 23:03:19 +0200

    2 state added

--- src/components/random-color/RandomColor.jsx
+++ src/components/random-color/RandomColor.jsx
@@ -1,11 +1,17 @@
 import { useState } from "react";
 
 export default function RandomColor() {
-  const [typeOfColor, setTypeOfColor] = useState();
-  const [] = useState();
+  const [typeOfColor, setTypeOfColor] = useState("hex");
+  const [color, setColor] = useState("#000000");
 
   return (
-    <div>
+    <div
+      style={{
+        width: "100vw",
+        height: "100vh",
+        background: color,
+      }}
+    >
       <button>Create HEX Color</button>
       <button>Create RGB Color</button>
       <button>Generate Random Color</button>
